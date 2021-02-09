f057f678206971f55016db96b65f5ed5ac766f7c
Blake McKenzie <02blake.mckenzie@gulllakecs.org>
2021-02-09 13:03:10 -0500

    stable

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
+import java.text.NumberFormat;
 
 /**
  * This app displays an order form to order coffee.
@@ -24,8 +25,13 @@ public class MainActivity extends AppCompatActivity {
      */
     public void submitOrder(View view) {
         display(1);
-    }
 
+
+    display(2);
+
+    displayPrice(2*5);
+
+}
     /**
      * This method displays the given quantity value on the screen.
      */
@@ -33,4 +39,13 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-}
+
+    {
+        /**
+         * This method displays the given price on the screen.
+         */
+        private void displayPrice ( int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+     }
+    }
