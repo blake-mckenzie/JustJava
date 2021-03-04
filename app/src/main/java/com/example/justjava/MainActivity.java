4b61dabf3203ab1764c5bc004ce3879f67bb562f
Blake McKenzie <02blake.mckenzie@gulllakecs.org>
2021-03-04 12:41:11 -0500

    Lesson 7 - Modified the String Message

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-       String priceMessage = "Free";
+       String priceMessage = "She said \"1 dollar\"";
        displayMessage(priceMessage);
 
    }
